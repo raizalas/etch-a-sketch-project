134be5ab529dcbfa6a384be3c5612d6ab403dc48
raizalas <ashari.raizal17@gmail.com>
2024-04-08 15:01:51 +0700

    created barebones grid with no functionality

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-const cdiv = document.querySelector('.grid-container');
+const GRIDSIZE = 600;
+let gridRows = 24;
+let gridColumns = 24;
 
-// Add 16 divs
-for (let i = 1; i < 257; i++) {
-  const div = document.createElement('div');
-  cdiv.appendChild(div);
+const gridArea = document.querySelector("#grid-area");
+gridArea.style.width = `${GRIDSIZE}px`;
+gridArea.style.height = `${GRIDSIZE}px`;
+
+function createCells() {
+  for (let i = 0; i < gridRows * gridColumns; i++) {
+    const gridCell = document.createElement("div");
+
+    gridCell.style.width = `${GRIDSIZE / gridColumns - 2}px`;
+    gridCell.style.height = `${GRIDSIZE / gridRows - 2}px`;
+    gridCell.classList.add("cell");
+
+    gridArea.appendChild(gridCell);
+  }
 }
+
+createCells()
