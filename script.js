515c6b10adff9f5ca86303956555ed8479db084d
raizalas <ashari.raizal17@gmail.com>
2024-04-08 19:41:59 +0700

    added slider to change the grid size

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 const GRIDSIZE = 600;
-let colorChange = 'black';
+let colorChange = "black";
 
 const gridArea = document.querySelector("#grid-area");
 gridArea.style.width = `${GRIDSIZE}px`;
 gridArea.style.height = `${GRIDSIZE}px`;
+const sliderContainer = document.querySelector(".slider-container");
+const slider = document.querySelector("#slider");
+const sliderValue = document.querySelector(".slider-value");
+
+sliderValue.textContent = `${slider.value} x ${slider.value}`;
 
 function changeBackgroundColor() {
-  this.style.backgroundColor = colorChange
+  this.style.backgroundColor = colorChange;
 }
 
 function createCells(gridSide) {
-  const gridSquareArea = gridSide * gridSide
-  const gridWidthHeight = `${GRIDSIZE / gridSide - 2}px`
+  const gridSquareArea = gridSide * gridSide;
+  const gridWidthHeight = `${GRIDSIZE / gridSide - 2}px`;
   for (let i = 0; i < gridSquareArea; i++) {
     const gridCell = document.createElement("div");
 
@@ -21,9 +26,20 @@ function createCells(gridSide) {
 
     gridArea.appendChild(gridCell);
 
-    gridCell.addEventListener('mouseover', changeBackgroundColor)
-   
+    gridCell.addEventListener("mouseover", changeBackgroundColor);
   }
 }
 
-createCells(16)
+function removeCells() {
+  while (gridArea.firstChild) {
+    gridArea.removeChild(gridArea.firstChild);
+  }
+}
+
+slider.oninput = function() {
+  sliderValue.textContent = `${slider.value} x ${slider.value}`;
+  removeCells();
+  createCells(this.value)
+}
+
+createCells(16);
